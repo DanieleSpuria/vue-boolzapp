739248ce7df71571b16a9d683dded33e79c45875
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-18 10:39:02 +0200

    add milestone2

---
+++ milestone2/assets/js/script.js
@@ -0,0 +1,28 @@
+const {createApp} = Vue;
+import contacts from './db.js';
+
+createApp({
+  data() {
+    return{
+      contacts,
+      userActive: {}
+    }
+  },
+
+  methods: {
+    active() {
+      this.contacts.forEach(contact => {
+        if (contact.visible) {
+          this.userActive = {
+            name: contact.name,
+            avatar: contact.avatar
+          }
+        }
+      })
+    }
+  },
+
+  mounted(){
+    this.active()
+  }
+}).mount('#app');
